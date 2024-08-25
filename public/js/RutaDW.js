51d83977b282ae77def3d7a7e609168802e9c50e
Brayan <159861819+020510bo@users.noreply.github.com>
2024-08-24 20:32:26 -0400

    creando repo

---
+++ public/js/RutaDW.js
@@ -0,0 +1,4 @@
+document.getElementById('webde').addEventListener('click', function() {
+    const pptUrl = 'https://docs.google.com/presentation/d/1BcdIAP_32iwPP5sykhenwW_bIQ1GPhK4/edit#slide=id.p1';
+    window.open(pptUrl, '_blank');
+});
---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+document.getElementById('openPPTButton').addEventListener('click', function() {
+    const pptUrl = 'https://docs.google.com/presentation/d/1xq5mkdF3tBAiMd_a6Wp2zFJR83clXC9o/edit#slide=id.p1';
+    window.open(pptUrl, '_blank');
+});
+
+
+document.getElementById('proyectos5').addEventListener('click', function() {
+    const pptUrl = 'https://docs.google.com/document/d/1R-THJ5iVRqcC7HHjZxncNs9yJMiuRxJa/edit';
+    window.open(pptUrl, '_blank');
+});
