10c8cde6d5b8b145cdc1a66038321ba902dad103
Brayan <159861819+020510bo@users.noreply.github.com>
2024-08-25 12:18:37 -0400

    corrigiendo errores

--- public/js/RutaDW.js
+++ public/js/RutaDW.js
@@ -4,6 +4,6 @@ document.getElementById('webde').addEventListener('click', function() {
 });
 
 document.getElementById('webAI').addEventListener('click', function() {
-    const pptUrl = 'https://docs.google.com/presentation/d/15RLHiUOyMWQECPFJmmJOvNPnqYjBdEM2/edit?usp=drive_web&ouid=106120840265932193924&rtpof=true';
+    const pptUrl = 'https://docs.google.com/presentation/d/15RLHiUOyMWQECPFJmmJOvNPnqYjBdEM2/edit#slide=id.p1';
     window.open(pptUrl, '_blank');
 });
