b4e6ed45fa0d673f9b2b84d2d914c5d1a7bd6eb2
Brayan <159861819+020510bo@users.noreply.github.com>
2024-08-24 21:42:48 -0400

    agregue programadores de AI

--- public/js/RutaDW.js
+++ public/js/RutaDW.js
@@ -2,3 +2,8 @@ document.getElementById('webde').addEventListener('click', function() {
     const pptUrl = 'https://docs.google.com/presentation/d/1BcdIAP_32iwPP5sykhenwW_bIQ1GPhK4/edit#slide=id.p1';
     window.open(pptUrl, '_blank');
 });
+
+document.getElementById('webAI').addEventListener('click', function() {
+    const pptUrl = 'https://docs.google.com/presentation/d/15RLHiUOyMWQECPFJmmJOvNPnqYjBdEM2/edit?usp=drive_web&ouid=106120840265932193924&rtpof=true';
+    window.open(pptUrl, '_blank');
+});
