86efb56cea3f294568f008cc4bf8d81c91340c54
Brayan <159861819+020510bo@users.noreply.github.com>
2024-10-09 19:13:43 -0400

    cursor

---
+++ public/cursor/app.js
@@ -0,0 +1,8 @@
+let tag1 = document.querySelector(".btn-caracteristicas");
+let tag2 = document.querySelector(".btn-Ventajas");
+let tag3 = document.querySelector(".btn-porque");
+let tag4 = document.querySelector(".descargar");
+
+tag1.addEventListener("click", () => {
+    window.location.href = "caracteristicas.html";
+});
